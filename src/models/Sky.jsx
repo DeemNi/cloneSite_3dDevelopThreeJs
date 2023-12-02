9b1d056f56020d55b7e0b8d1832b6f0b288fa275
Nikita <blasthourly@gmail.com>
2023-12-02 17:26:16 +0200

    adding animation to all layer of the site. Sky + Bird + Plane + Main Island Scene Rotation

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {useRef} from "react";
+
 import { useGLTF } from "@react-three/drei"
+import { useFrame } from "@react-three/fiber";
 
 import skyScene from '../assets/3d/sky.glb'
 
-const Sky = () => {
+const Sky = ({isRotating}) => {
     const sky = useGLTF(skyScene);
+    const skyRef = useRef();
+
+    useFrame((_, delta) => {
+        if(isRotating){
+            //change number for modifire speed rotation of sky
+            skyRef.current.rotation.y += 0.15 * delta;
+        }
+    })
     return (
-        <mesh>
+        <mesh ref={skyRef}>
             <primitive object={sky.scene} />
         </mesh>
     )
