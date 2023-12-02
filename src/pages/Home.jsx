a94a9f4dbfa8a9a814b3b5c45b492de7f4c3aa33
Nikita <blasthourly@gmail.com>
2023-12-02 03:22:24 +0200

    Adding asset folder with 3d object. Create 2 components - sky and island (this is 3d models)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
 import { Canvas } from "@react-three/fiber"
 import Loader from "../components/Loader"
+import { Suspense } from "react"
+import Island from "../models/island"
+import Sky from "../models/Sky"
 
 const Home = () => {
+
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0]
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+      screenPosition = [0, -6.5, -43];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+    return [screenScale, screenPosition, rotation];
+  }
+
+  const [islandScale, islandPosition, rotation] = adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -13,11 +33,16 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}>
 
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight skyColor='#b1e1ff' groundColor={'#000000'} intensity={1} />
+
+          <Sky />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={rotation}
+          />
         </Suspense>
 
       </Canvas>
@@ -26,4 +51,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;
